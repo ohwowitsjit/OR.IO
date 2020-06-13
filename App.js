376ff26bec122d42c1051d8bfad51338fe0af6ca
BuildTools <unconfigured@null.spigotmc.org>
2020-06-13 22:05:43 +0800

    work permission and init audiorecord

--- App.js
+++ App.js
@@ -1,36 +1,73 @@
 //Import all libraries and dependencies
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, PermissionsAndroid } from "react-native";
 import AudioRecord from "react-native-audio-record";
-import { check, PERMISSIONS, RESULTS } from 'react-native-permissions';
+
+
+const requestRecordPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      {
+        title: "OR.IO Permissions",
+        message:
+          "We need to use your microphone " +
+          "todo what we do.",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK"
+      }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("RECORD AUDIO ENABLED");
+    } else {
+      console.log("DENIED");
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
 
 //App Component
-const App = () => {
+class App extends Component {
 
-  //Define options object for AudioRecord
-  const options = {
-    sampleRate: 16000,
-    channels: 1,
-    bitsPerSample: 16,
-    wavFile: 'audio.wav'
+  async componentDidMount() {
+    //Get permission to use microphone
+    requestRecordPermission();
+
+    //Define options object for AudioRecord
+    const options = {
+      sampleRate: 16000,
+      channels: 1,
+      bitsPerSample: 16,
+      wavFile: 'audio.wav'
+    };
+
+    //Init AudioRecord
+    AudioRecord.init(options);
+
+    AudioRecord.start();
+
+    audioFile = await AudioRecord.stop();
+
   };
-  //Init AudioRecord
-  //AudioRecord.init(options)
 
 
 
 
   //Render app components
-  return (
-    <View style={styles.background} >
-      <View>
-        <Text style={styles.logo}>OR.IO</Text>
+  render() {
+    return (
+      <View style={styles.background} >
+        <View>
+          <Text style={styles.logo}>OR.IO</Text>
+        </View>
+        <View style={styles.recArea}>
+          <Text style={styles.centerText}>RECORDING...</Text>
+        </View>
       </View>
-      <View style={styles.recArea}>
-        <Text style={styles.centerText}>RECORDING...</Text>
-      </View>
-    </View>
-  );
+    );
+  };
+
 };
 
 //Styling of components
@@ -50,6 +87,7 @@ const styles = StyleSheet.create({
     fontSize: 80,
     padding: 0,
   },
+  //Styling for recording text's wrapper
   recArea: {
     backgroundColor: "#F1D302",
     minHeight: 40,
